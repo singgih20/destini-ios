283cd3a3b72c6bff2be18d6bfff1e3a478a4ef7c
Singgih <singgihaji@weekendinc.com>
2024-10-11 22:47:12 +0700

    add mvc

--- Controller/ViewController.swift
+++ Controller/ViewController.swift
@@ -9,16 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var storyBrain = StoryBrain()
+    
     @IBOutlet weak var storyLabel: UILabel!
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateUI()
     }
 
-
+    @IBAction func choiceMade(_ sender: UIButton) {
+        storyBrain.nextStory(sender.currentTitle ?? "")
+        updateUI()
+    }
+    
+    func updateUI() {
+        let numberOfExample = storyBrain.getNumberOfStories()
+        storyLabel.text = storyBrain.stories[numberOfExample].title
+        choice1Button.setTitle(storyBrain.stories[numberOfExample].choice1, for: .normal)
+        choice2Button.setTitle(storyBrain.stories[numberOfExample].choice2, for: .normal)
+    }
 }
 
--- Model/StoryBrain.swift
+++ Model/StoryBrain.swift
@@ -9,3 +9,23 @@
 import Foundation
 
 
+struct StoryBrain {
+    var stories = [
+        Story(title: "You see a fork in the road", choice1: "Take a left", choice2: "Take a right"),
+        Story(title: "You see a tiger", choice1: "Shout for help", choice2: "Play dead"),
+        Story(title: "You find a treasure chest", choice1: "Open it", choice2: "Check for traps"),
+    ]
+    var numberOfStories = 0
+    
+    mutating func nextStory(_ userChoice: String) {
+        if userChoice == "Take a left" {
+            numberOfStories = 1
+        } else {
+            numberOfStories = 2
+        }
+    }
+    
+    func getNumberOfStories() -> Int {
+        return numberOfStories
+    }
+}
